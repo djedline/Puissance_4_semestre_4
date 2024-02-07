ebef8f79d81dba9190dc6b46e2e58030fd9b4c28
Tany CATALA BAILLY <catalatany@gmail.com>
2024-02-07 16:08:19 +0100

    version et bugger qui marche pas de minimax

--- Objets/MinMax.py
+++ Objets/MinMax.py
@@ -1,11 +1,24 @@
 #Classe MinMax
 class MinMax() : 
 
+    #Méthode qui defini la valeur d'un plateau
+    def valeur(plateau) : 
+        return 10
     #Méthode qui détermine le minimum d'un tableau
-    def min(tableau) :
-
+    def min(p,d) :
+        if p.isFeuille or d==0 :
+            return MinMax.valeur(p)
+        else :
+            for lePlateau in p.enfant :
+                return max(lePlateau, d-1)
     #Méthode qui détermine le maximum d'un tableau
-    def max(tableau) :  
-
+    def max(p,d) :  
+        if p.isFeuille or d==0 :
+            return MinMax.valeur(p)
+        else :
+            for lePlateau in p.enfant :
+                return min(lePlateau, d-1)
     #méthode qui donne me mini ou maxi du tableau
     def minimax(p,d) :
+        for lePlateau in p.enfant :
+                return max(lePlateau, d-1)
